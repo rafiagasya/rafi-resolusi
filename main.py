726e46a578560557b8c470be4124647d2d34773d
rafi agasya <132184120+rafiagasya@users.noreply.github.com>
2024-10-12 09:44:48 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a Note/Comment 
+#print("Hello there everyone learning!")
+
+#print("Item coast:")
+#Give a 20% discount
+#print((25*4) * 0.8 )
+
+
+
+
+
+print("Funds to send:", 200*5 + 600/5 + 750-75)
+
+#print(57 % 3)
+#print(57 % 4)
+
+print(30/9)
+print(30//9)
+
+
---
+++ variable.py
@@ -0,0 +1,43 @@
+# Create 4 variables travelling. flight_cost, insurance, passport, baggage.
+# Create a variable to calculate the cost of all of them.
+# print off the total cost with strings and variables.
+# Note -> There are 5 people travelli.
+
+flight_cost = 455
+insurance = 19.99
+passport = 110
+baggage = 45
+
+total = (flight_cost + insurance + passport + baggage) * 5
+# print("Total cost:",total)
+
+# Create 4 variables for ticket price. 1 variable is for the number of adults & 1 for children.
+# create 1 variable for the adult price and 1 for the child price.
+# Create a variable to calculate the total
+# print off the total with strings and variables
+
+adult = 2
+children = 2
+
+adult_price = 55
+child_price = 27
+
+total = (adult * adult_price) + (children * child_price)
+
+print("Six flags total:", total)
+
+
+# A resort offers the normal price of 500 per night, a family wants to stay for 1 week
+# the resort is offering a special promo of 30% off
+# print off the last minute trip price with strings and variables
+
+price = 500
+days = 7
+
+# Total sum in price * and days for example
+# From basic coding python import sum
+
+extra = input("Price of adds-ons:")
+extra = int(extra)
+total = 1000 + extra
+print("Total price:", total)
